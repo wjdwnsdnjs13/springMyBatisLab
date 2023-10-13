4cd29a77f8f08816ea680a9150d7834e0d1f8ad3
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2023-10-14 00:24:58 +0900

    create new project

---
+++ src/main/java/com/springMyBatisLab/SpringMyBatisLabApplication.java
@@ -0,0 +1,13 @@
+package com.springMyBatisLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMyBatisLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMyBatisLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springMyBatisLab/SpringMyBatisLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springMyBatisLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMyBatisLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
